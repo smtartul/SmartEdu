5f993c4827f3d35dbf6348ed725cc7ee036a29b3
Samet ARTUL <smtartul@gmail.com>
2022-02-23 00:19:43 +0300

    slugify routes courses and single course pages created

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express=require('express')
+const mongoose=require('mongoose')
 const pageRoute=require('./routes/pageRoute')
+const courseRoute=require('./routes/courseRoute')
+
 
 const app=express();
 
+//connectdb mongo,
+mongoose.connect('mongodb://localhost:27017/smartedu-db').then(()=>{
+    console.log('Db connected succesfully!');
+});
 //Template engine definition for ejs
 app.set("view engine","ejs")
 
 //Middlewares definitions
 app.use(express.static("public"));
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 //routes
 app.use('/',pageRoute)
+app.use('/courses',courseRoute)
 
 const port=5000;
 app.listen(port,()=>{
